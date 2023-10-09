c68908155e2ecf31cc530a8616ec778cd5478809
Dmitry Openkov <dimaopen@gmail.com>
2023-10-09 18:25:50 +0300

    Using cpus-per-task sbatch parameter. Singularity uses names of docker images.

--- hpc/job_runner.sh
+++ hpc/job_runner.sh
@@ -4,9 +4,10 @@ RANDOM_PART="$(tr -dc A-Z0-9 </dev/urandom | head -c 8)"
 DATETIME="$(date "+%Y.%m.%d-%H.%M.%S")"
 JOB_NAME="$RANDOM_PART.$DATETIME"
 
-PARTITION="es1"
-QOS="es_normal"
-MEMORY_LIMIT="80"
+PARTITION="lr7"
+QOS="lr_normal"
+NUM_CPUS=50
+MEMORY_LIMIT="240"
 ACCOUNT="pc_beamcore"
 JOB_LOG_FILE_PATH="/global/scratch/users/$USER/pilates_logs/log_${DATETIME}_${RANDOM_PART}.log"
 EXPECTED_EXECUTION_DURATION="3-00:00:00"
@@ -15,6 +16,7 @@ mkdir -p "/global/scratch/users/$USER/pilates_logs"
 
 sbatch --partition="$PARTITION" \
     --exclusive \
+    --cpus-per-task="$NUM_CPUS" \
     --mem="${MEMORY_LIMIT}G" \
     --qos="$QOS" \
     --account="$ACCOUNT" \
