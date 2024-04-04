17d1a624eb65ab96da364557ec8fb150ff072f17
Zach Needell <zaneedell@lbl.gov>
2024-04-04 14:22:07 -0700

    bugfixes

--- hpc/job.sh
+++ hpc/job.sh
@@ -5,5 +5,5 @@ module load python/3.8.8
 #pip install --user geopandas
 export PYTHONPATH=`python -m site --user-site`:$PYTHONPATH
 cd /global/scratch/users/$USER/sources/PILATES
-python run.py -v -c="$1" -s="$2"
+python run.py -v -c "$1" -S "$2"
 
