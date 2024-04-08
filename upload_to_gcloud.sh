41f34e3932192ec7c5e49602007b70640e1d12b2
jlaz <jlaz@berkeley.edu>
2024-04-08 17:30:24 +0000

    Cruise phase2 debugging

--- upload_to_gcloud.sh
+++ upload_to_gcloud.sh
@@ -9,11 +9,11 @@ if  [ $# -eq 2 ]
         gcloud alpha storage cp -r -n pilates/activitysim/output/final* gs://cruise-outputs/$2/activitysim/
         gcloud alpha storage cp -r -n pilates/activitysim/output/year* gs://cruise-outputs/$2/activitysim/
         gcloud alpha storage cp -r -n pilates/activitysim/output/pipeline.h5 gs://cruise-outputs/$2/activitysim/pipeline.h5
-        gcloud alpha storage cp -r -n pilates/activitysim/data/ gs://cruise-outputs/$2/activitysim/data/
+        gcloud alpha storage cp -r  pilates/activitysim/data/ gs://cruise-outputs/$2/activitysim/data/
         gcloud alpha storage cp -r -n pilates/postprocessing/output/ gs://cruise-outputs/$2/inexus/
-        gcloud alpha storage cp -r -n pilates/postprocessing/MEP/ gs://cruise-outputs/$2/MEP/
+        #gcloud alpha storage cp -r -n pilates/postprocessing/MEP/ gs://cruise-outputs/$2/MEP/
         gcloud alpha storage cp -n log_pilates_$1.out gs://cruise-outputs/$2/log_pilates_$1.out
-        rclone copy pilates/postprocessing/MEP/ s3://cruise-outputs/pilates-outputs/$2/MEP/
+        #rclone copy pilates/postprocessing/MEP/ s3://cruise-outputs/pilates-outputs/$2/MEP/
 
 else
     echo "Please provide a region (e.g. 'austin' or 'sfbay') and GCloud directory name"
