097c42437a14514340aea02e8285e134d0921bbe
jlaz <jlaz@berkeley.edu>
2024-04-23 17:59:28 -0700

    debug

--- pilates/utils/update_ridehail_fleets.py
+++ pilates/utils/update_ridehail_fleets.py
@@ -9,7 +9,7 @@ from shapely import wkt
 
 def update_fleet_files(trips_df, settings):
     FLEET_MIX_FILENAME = settings['TNCfleetMixFilename']
-    BEAM_INPUT_PATH = '{}{}'.format(settings['beam_local_input_folder'],settings['region'])
+    BEAM_INPUT_PATH = settings['beamInputPath']
     TAZ_FILENAME = '{}/{}/{}'.format(BEAM_INPUT_PATH ,settings['beam_router_directory'],settings['beam_geoms_fname'])
     #settings['tazFilename']
     ZIP_FILENAME = settings['zipFilename']
