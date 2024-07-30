2bfcc2679ccc39a90f27d6351e761f50552d250a
Zach Needell <zaneedell@lbl.gov>
2024-07-30 13:44:25 -0700

    bug

--- workflow_state.py
+++ workflow_state.py
@@ -130,7 +130,7 @@ class WorkflowState:
     def read_current_stage(cls, file_loc):
         if not os.path.exists(file_loc):
             logger.info("Creating new stage info at {}".format(file_loc))
-            return [None, None, None, None]
+            return [None, None, None, None, None]
         with open(file_loc, encoding="utf-8") as f:
             data = yaml.load(f, Loader=yaml.FullLoader)
             data = data if data is not None else {}
