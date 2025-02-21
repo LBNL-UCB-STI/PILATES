c343fceeb65f95351ec2aed5ac9432f4b1a9dc84
Haitam Laarabi <10712736+htmlrb@users.noreply.github.com>
2025-02-21 15:48:37 -0800

    test6

--- hpc/job.sh
+++ hpc/job.sh
@@ -5,23 +5,27 @@ module load gcc/11.4.0
 module load geos/3.12.0
 module load python/3.10.12
 
-# First uninstall everything
-pip uninstall -y numpy pandas geopandas shapely pyproj pygeos tables numexpr bottleneck matplotlib
+# First, remove all relevant packages from user space
+rm -rf ~/.local/lib/python3.10/site-packages/numpy*
+rm -rf ~/.local/lib/python3.10/site-packages/pandas*
+rm -rf ~/.local/lib/python3.10/site-packages/geopandas*
+rm -rf ~/.local/lib/python3.10/site-packages/shapely*
+rm -rf ~/.local/lib/python3.10/site-packages/pyproj*
+rm -rf ~/.local/lib/python3.10/site-packages/pygeos*
+rm -rf ~/.local/lib/python3.10/site-packages/tables*
 
-# Install specific version of numpy first
-pip install --user numpy==1.24.4
+# Install exact versions that work together
+pip install --user --no-deps numpy==1.23.5
+pip install --user --no-deps pandas==1.5.3
+pip install --user --no-deps shapely==1.8.5
+pip install --user --no-deps pyproj==3.6.1
+pip install --user --no-deps pygeos==0.14
+pip install --user --no-deps geopandas==0.11.1
+pip install --user --no-deps tables==3.8.0
+pip install --user --no-deps matplotlib==3.7.1
 
-# Then install other packages specifying versions that work with numpy 1.24
-pip install --user pandas==1.5.3
-pip install --user shapely==1.8.5
-pip install --user pyproj==3.6.1
-pip install --user pygeos==0.14
-pip install --user geopandas==0.11.1
-pip install --user tables==3.8.0
-pip install --user matplotlib==3.7.1
-
-# If you need tables package:
-pip install --user --force-reinstall tables
+# Install dependencies
+pip install --user python-dateutil pytz numexpr bottleneck
 
 export PYTHONPATH=`python -m site --user-site`:$PYTHONPATH
 cd /global/scratch/users/$USER/sources/PILATES
