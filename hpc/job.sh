114ec95a16dfd47f57ac6d2eee30dfb5372778c9
Haitam Laarabi <10712736+htmlrb@users.noreply.github.com>
2025-02-21 14:20:59 -0800

    update

--- hpc/job.sh
+++ hpc/job.sh
@@ -5,13 +5,13 @@
 #conda activate PILATES
 #conda install -c conda-forge geopandas h5py openmatrix pytables numpy docker-py tqdm pyyaml psutil cloudpickle
 module load python/3.10.12
-#python -m pip uninstall shapely
-#python -m pip install --user shapely
-#python -m pip install --user openmatrix
-#python -m pip install --user pygeos
-#python -m pip install --user geopandas
-#python -m pip install --user table
-#python -m pip install --user PyYAML
+pip uninstall shapely
+pip install --user shapely
+pip install --user openmatrix
+pip install --user pygeos
+pip install --user geopandas
+pip install --user table
+pip install --user PyYAML
 export PYTHONPATH=`python -m site --user-site`:$PYTHONPATH
 cd /global/scratch/users/$USER/sources/PILATES
 echo "$1"
