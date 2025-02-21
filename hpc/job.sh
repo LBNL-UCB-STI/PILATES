426a5020b20cddf6b4a3d2f9d00ae611e0940f45
Haitam Laarabi <10712736+htmlrb@users.noreply.github.com>
2025-02-21 15:06:56 -0800

    test2

--- hpc/job.sh
+++ hpc/job.sh
@@ -6,10 +6,13 @@
 #conda install -c conda-forge geopandas h5py openmatrix pytables numpy docker-py tqdm pyyaml psutil cloudpickle
 module load python/3.10.12
 pip uninstall -y shapely geopandas pygeos pyproj
-pip install --user shapely
-pip install --user pyproj
-pip install --user pygeos
-pip install --user geopandas
+pip install --user shapely==1.8.5  # Match the version that was previously installed
+pip install --user pyproj==3.6.1   # Match the version from the log
+pip install --user pygeos==0.14    # Match the version from the log
+pip install --user geopandas==0.11.1  # Match the version from the log
+conda create -n geo_env
+conda activate geo_env
+conda install -c conda-forge geos
 #pip uninstall shapely
 #pip uninstall geopandas
 #pip install --user shapely
