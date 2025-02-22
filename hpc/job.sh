11250eb929eed707105153930c1646f71c7eed91
Haitam Laarabi <10712736+htmlrb@users.noreply.github.com>
2025-02-21 17:14:52 -0800

    adding comment

--- hpc/job.sh
+++ hpc/job.sh
@@ -4,6 +4,21 @@
 module load gcc/11.4.0
 module load python/3.10.12-gcc-11.4.0
 
+
+## Create directories for local installation
+#mkdir -p $HOME/.local/src
+#mkdir -p $HOME/.local/geos
+#
+## Download and build GEOS
+#cd $HOME/.local/src
+#wget https://download.osgeo.org/geos/geos-3.12.0.tar.bz2
+#tar xjf geos-3.12.0.tar.bz2
+#cd geos-3.12.0
+#./configure --prefix=$HOME/.local/geos
+#make -j4
+#make install
+
+
 # Set up environment variables for GEOS
 export LD_LIBRARY_PATH=$HOME/.local/geos/lib64:$LD_LIBRARY_PATH
 export PATH=$HOME/.local/geos/bin:$PATH
