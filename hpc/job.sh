43c4d1a6bed89df6513cea464d0cde67a4a14e87
Haitam Laarabi <10712736+htmlrb@users.noreply.github.com>
2025-02-21 16:35:04 -0800

    test10

--- hpc/job.sh
+++ hpc/job.sh
@@ -4,22 +4,10 @@
 module load gcc/11.4.0
 module load python/3.10.12-gcc-11.4.0
 
-# Create directories for local installation
-mkdir -p $HOME/.local/src
-mkdir -p $HOME/.local/geos
-
-# Download and build GEOS
-cd $HOME/.local/src
-wget https://download.osgeo.org/geos/geos-3.12.0.tar.bz2
-tar xjf geos-3.12.0.tar.bz2
-cd geos-3.12.0
-./configure --prefix=$HOME/.local/geos
-make -j4
-make install
-
-# Set up environment variables
-export LD_LIBRARY_PATH=$HOME/.local/geos/lib:$LD_LIBRARY_PATH
+# Set up environment variables for GEOS
+export LD_LIBRARY_PATH=$HOME/.local/geos/lib64:$LD_LIBRARY_PATH
 export PATH=$HOME/.local/geos/bin:$PATH
+export GEOS_CONFIG=$HOME/.local/geos/bin/geos-config
 
 # Clean previous installations
 rm -rf ~/.local/lib/python3.10/site-packages/numpy*
@@ -30,18 +18,24 @@ rm -rf ~/.local/lib/python3.10/site-packages/pyproj*
 rm -rf ~/.local/lib/python3.10/site-packages/pygeos*
 rm -rf ~/.local/lib/python3.10/site-packages/tables*
 
-# Install Python packages
-pip install --user --no-deps numpy==1.23.5
-pip install --user --no-deps pandas==1.5.3
-pip install --user --no-binary shapely shapely==1.8.5
-pip install --user --no-deps pyproj==3.6.1
-pip install --user --no-deps pygeos==0.14
-pip install --user --no-deps geopandas==0.11.1
-pip install --user --no-deps tables==3.8.0
-pip install --user --no-deps matplotlib==3.7.1
+# Force reinstall all packages
+pip install --user --force-reinstall --no-deps numpy==1.23.5
+pip install --user --force-reinstall --no-deps pandas==1.5.3
+pip install --user --force-reinstall --no-binary shapely shapely==1.8.5
+pip install --user --force-reinstall --no-deps pyproj==3.6.1
+pip install --user --force-reinstall --no-deps pygeos==0.14
+pip install --user --force-reinstall --no-deps geopandas==0.11.1
+pip install --user --force-reinstall --no-deps tables==3.8.0
+pip install --user --force-reinstall --no-deps matplotlib==3.7.1
 
 # Install dependencies
-pip install --user python-dateutil pytz numexpr bottleneck
+pip install --user --force-reinstall python-dateutil pytz numexpr bottleneck
+
+# Verify GEOS installation
+echo "Checking GEOS installation:"
+ls -l $HOME/.local/geos/lib64/libgeos*
+echo "GEOS_CONFIG location: $GEOS_CONFIG"
+$GEOS_CONFIG --version
 
 # Set PYTHONPATH
 export PYTHONPATH=`python -m site --user-site`:$PYTHONPATH
