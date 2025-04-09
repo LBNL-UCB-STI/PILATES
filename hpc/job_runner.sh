ae263e2986c7402062ba50a3f13bc6b8bace2338
zneedell <zneedell@gmail.com>
2025-04-09 16:08:34 -0700

    use max cpus and memory for lr7

--- hpc/job_runner.sh
+++ hpc/job_runner.sh
@@ -22,8 +22,8 @@ done
 
 PARTITION="lr7"
 QOS="lr_normal"
-NUM_CPUS=50
-MEMORY_LIMIT="200"
+NUM_CPUS=56
+MEMORY_LIMIT="256"
 ACCOUNT="pc_beamcore"
 JOB_LOG_FILE_PATH="/global/scratch/users/$USER/pilates_logs/log_${DATETIME}_${RANDOM_PART}.log"
 EXPECTED_EXECUTION_DURATION="3-00:00:00"
