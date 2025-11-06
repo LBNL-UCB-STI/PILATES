5b205ff21ee45ee6d25b6fef5fb8054a39f2e81e
Haitam Laarabi <10712736+htmlrb@users.noreply.github.com>
2025-11-06 12:35:58 -0800

    update job_runner

--- hpc/job_runner.sh
+++ hpc/job_runner.sh
@@ -6,23 +6,40 @@ JOB_NAME="$RANDOM_PART.$DATETIME"
 
 settings_file="settings.yaml"
 stage_file="current_stage_${JOB_NAME}.yaml"
-while getopts :c:s: name
+
+# Default values
+PARTITION="lr8"
+QOS="lr8_normal"
+NUM_CPUS=128
+MEMORY_LIMIT="700"
+
+while getopts :c:s:p: name
 do
     case $name in
     c)    cflag=1
           settings_file="$OPTARG";;
     s)    sflag=1
           stage_file="$OPTARG";;
-    ?)   printf "Usage: %s: [-c settings/config file loc] [-s stage file loc] args\n" $0
+    p)    pflag=1
+          partition_arg="$OPTARG";;
+    ?)   printf "Usage: %s: [-c settings/config file loc] [-s stage file loc] [-p partition] args\n" $0
           exit 2;;
     esac
 done
 
+# Set values based on partition argument
+if [ "$partition_arg" = "lr8" ]; then
+    PARTITION="lr8"
+    QOS="lr8_normal"
+    NUM_CPUS=128
+    MEMORY_LIMIT="700"
+elif [ "$partition_arg" = "lr7" ]; then
+    PARTITION="lr7"
+    QOS="lr_normal"
+    NUM_CPUS=56
+    MEMORY_LIMIT="500"
+fi
 
-PARTITION="lr8"
-QOS="lr8_normal"
-NUM_CPUS=128
-MEMORY_LIMIT="700"
 ACCOUNT="pc_beamcore"
 JOB_LOG_FILE_PATH="/global/scratch/users/$USER/pilates_logs/log_${DATETIME}_${RANDOM_PART}.log"
 EXPECTED_EXECUTION_DURATION="3-00:00:00"
