7d5edb61d1307ec659286abbd070dafd1b58d78b
Haitam Laarabi <10712736+htmlrb@users.noreply.github.com>
2026-01-05 15:59:46 -0800

    adding beam memory variable to yaml files

--- hpc/job_runner.sh
+++ hpc/job_runner.sh
@@ -4,29 +4,32 @@ RANDOM_PART="$(tr -dc A-Z0-9 </dev/urandom | head -c 8)"
 DATETIME="$(date "+%Y.%m.%d-%H.%M.%S")"
 JOB_NAME="$RANDOM_PART.$DATETIME"
 
-settings_file="settings.yaml"
-stage_file="current_stage_${JOB_NAME}.yaml"
+template_file="settings_template.yaml" # Template file with variables
+settings_file="settings.yaml" # This file will be generated
+stage_file="current_stage_${JOB_NAME}.yaml" # Current stage file
 
 # Default values
 PARTITION="lr7"
 QOS="lr_normal"
 NUM_CPUS=56
 MEMORY_LIMIT="500"
+BEAM_MEMORY="400g"
 # PARTITION="lr8"
 # QOS="lr8_normal"
 # NUM_CPUS=128
 # MEMORY_LIMIT="700"
+# BEAM_MEMORY="600g"
 
 while getopts :c:s:p: name
 do
     case $name in
     c)    cflag=1
-          settings_file="$OPTARG";;
+          template_file="$OPTARG";; # Command line argument for settings template file
     s)    sflag=1
-          stage_file="$OPTARG";;
+          stage_file="$OPTARG";; # Command line argument for stage file
     p)    pflag=1
-          partition_arg="$OPTARG";;
-    ?)   printf "Usage: %s: [-c settings/config file loc] [-s stage file loc] [-p partition] args\n" $0
+          partition_arg="$OPTARG";; # Partition argument
+    ?)   printf "Usage: %s: [-c settings template file loc] [-s stage file loc] [-p partition] args\n" $0
           exit 2;;
     esac
 done
@@ -36,20 +39,30 @@ if [ "$partition_arg" = "lr8" ]; then
     PARTITION="lr8"
     QOS="lr8_normal"
     NUM_CPUS=128
-    MEMORY_LIMIT="700"
+    MEMORY_LIMIT="700" # In GB
+    BEAM_MEMORY="600g"
 elif [ "$partition_arg" = "lr7" ]; then
     PARTITION="lr7"
     QOS="lr_normal"
     NUM_CPUS=56
-    MEMORY_LIMIT="500"
+    MEMORY_LIMIT="500" # In GB
+    BEAM_MEMORY="400g"
 fi
+
+# Export the BEAM_MEMORY environment variable
+export BEAM_MEMORY
+
+# Create the final settings file from the template
+envsubst < "$template_file" > "$settings_file"
 
 ACCOUNT="pc_envisionai"
 JOB_LOG_FILE_PATH="/global/scratch/users/$USER/pilates_logs/log_${DATETIME}_${RANDOM_PART}.log"
 EXPECTED_EXECUTION_DURATION="3-00:00:00"
 
+# Create logs directory if it does not exist
 mkdir -p "/global/scratch/users/$USER/pilates_logs"
 
+# Submit the job
 sbatch --partition="$PARTITION" \
     --exclusive \
     --cpus-per-task="$NUM_CPUS" \
@@ -61,4 +74,5 @@ sbatch --partition="$PARTITION" \
     --time="$EXPECTED_EXECUTION_DURATION" \
     job.sh "$settings_file" "$stage_file"
 
+# Print the job log file path
 echo $JOB_LOG_FILE_PATH
